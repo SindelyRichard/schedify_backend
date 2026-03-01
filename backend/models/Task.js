75a822a91a8bb7df1141e8c914d3e8543dfa8e38
Sindely Richárd <113032480+SindelyRichard@users.noreply.github.com>
2026-03-01 15:21:13 +0100

    Add lastCompletedDate to Task schema
    Added lastCompletedDate field to Task schema.

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -5,7 +5,9 @@ const TaskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
     daily: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    lastCompletedDate: { type: Date, default: null }
 });
+
 
 module.exports = mongoose.model('Task', TaskSchema);
