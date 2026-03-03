0778daa69576bc46bb592d9713e386af8203991d
Sindely Richárd <113032480+SindelyRichard@users.noreply.github.com>
2026-03-03 17:34:16 +0100

    Refactor Task model schema in Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     userId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
     title: { type: String, required: true },
-    completed: { type: Boolean, default: false },
     daily: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     lastCompletedDate: { type: Date, default: null }
@@ -11,3 +10,4 @@ const TaskSchema = new mongoose.Schema({
 
 
 module.exports = mongoose.model('Task', TaskSchema);
+
