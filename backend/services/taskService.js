c78c866cdad39b82a996eb5d5062bcb83f7a3190
SindelyRichard <rsindely@gmail.com>
2026-03-19 22:40:49 +0100

    fix: taskService addTask method

--- backend/services/taskService.js
+++ backend/services/taskService.js
@@ -43,15 +43,14 @@ async function dailyTasks(username) {
 
 async function addTask(username, title) {
     try {
-        const daily = false;
         const user = await User.findOne({ username });
         if (!user) return { success: false, message: 'User not found' };
-        const taskCount = await Task.countDocuments({ userId: user._id, daily: false });
+        const taskCount = await Task.countDocuments({ userId: user._id });
 
         if (taskCount >= 5) {
             return { success: false, message: 'Reached maximum task!' };
         }
-        const task = new Task({ userId: user._id, title, daily });
+        const task = new Task({ userId: user._id, title });
         await task.save();
         return { success: true, task };
     } catch (err) {
