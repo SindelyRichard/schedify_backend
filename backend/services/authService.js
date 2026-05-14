7bb778c4227f0192f0b5fe2ed8d308751b841009
SindelyRichard <rsindely@gmail.com>
2026-05-14 23:41:05 +0200

    fix

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -49,7 +49,9 @@ async function loginUser(username, password) {
 }
 
 const transport = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWD
